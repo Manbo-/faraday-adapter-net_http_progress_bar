8368b5bbc7c7d9595ced20f97b9d3809a5b64ab0
Manbo- <Manbo-@server.fake>
2013-12-28 01:42:08 +0900

    refactor

--- lib/faraday/adapter/net_http_progress_bar/core.rb
+++ lib/faraday/adapter/net_http_progress_bar/core.rb
@@ -4,28 +4,42 @@ module Faraday
        Faraday.register_middleware(:adapter, net_http_progress_bar: NetHttpProgressBar)
 
       def perform_request(http, env)
-        if :get == env[:method] and !env[:body]
-          body = ""
-          response = http.request_get(env[:url].request_uri, env[:request_headers]) do |res|
-            response = res
-            format = "%t: |%B| %p%% %c/%C %E" if length = res["Content-Length"]
-            length = length ? length.to_i : nil
-            progress_bar = ProgressBar.create(title: env[:url], total: length, format: format)
+        http.request create_request(env) unless :get == env[:method] and !env[:body]
 
-            res.read_body do |fragment|
-              body << fragment
-              progress_bar.progress += fragment.length
-            end
-
-            unless length
-              progress_bar.total = body.size + 1
-              progress_bar.finish
-            end
+        body = ""
+        response = http.request_get(env[:url].request_uri, env[:request_headers]) do |res|
+          progress_bar = setup_progress_bar(env, res)
+          res.read_body do |fragment|
+            body << fragment
+            increment_progress_bar(progress_bar, fragment)
           end
-          response.body = body
-          response
-        else
-          http.request create_request(env)
+          finish_progress_bar(progress_bar, content_length(res), body)
+        end
+        response.body = body
+        response
+      end
+
+      private
+
+      def setup_progress_bar(env, response)
+        format = "%t: |%B| %p%% %c/%C %E" if content_length(response)
+        ProgressBar.create(title: env[:url], total: content_length(response), format: format)
+      end
+
+      def content_length(response)
+        response["Content-Length"] ? response["Content-Length"].to_i : nil
+      end
+
+      def increment_progress_bar(progress_bar, fragment)
+        progress_bar.progress += fragment.length
+      rescue # body length > content length 
+        return
+      end
+
+      def finish_progress_bar(progress_bar, content_length, body)
+        unless content_length
+          progress_bar.total = body.length + 1
+          progress_bar.finish
         end
       end
     end
--- lib/faraday/adapter/net_http_progress_bar/version.rb
+++ lib/faraday/adapter/net_http_progress_bar/version.rb
@@ -1,7 +1,7 @@
 module Faraday
   class Adapter
     class NetHttpProgressBar
-      VERSION = "0.0.1"
+      VERSION = "0.0.2"
     end
   end
 end
